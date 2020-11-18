8be0fd5b1adadd8f9cb787622b7af0f09edc2707
rakshith <rakshit.s.gowda@gmail.com>
2020-11-18 10:42:09 +0530

    firstcomit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
+import './index.css'
+const books = [{
+  id: 1,
+  img: 'https://m.media-amazon.com/images/I/710jnzKlDTL._AC_UY218_.jpg',
+  title: 'i love you to the moon and back',
+  author: 'Amelia Hepworth',
+}, {
+  id: 2,
+  img: 'https://m.media-amazon.com/images/I/711hfHGE+cL._AC_UL320_.jpg',
+  title: 'Great storiess fro children',
+  author: 'Anisoscm dnidc',
+}, {
+  id: 3,
+  img: 'https://m.media-amazon.com/images/I/51G-+iRKaIL._AC_UY218_.jpg',
+  title: 'Great storiess fro children',
+  author: 'Anisoscm dnidc',
+}];
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function BookList() {
+  return (
+    <section className='booklist'>
+      {books.map((book, index) => {
+        return (<Book key={book.id} {...book}></Book>)
+      })}
+    </section>
+  )
+}
+const Book = ({ img, title, author }) => {
+  const clickHandler = () => {
+    alert('hello world');
+  };
+  const complexExample = (author) => {
+    console.log(author);
+  }
+  return (
+    <article className='book' onMouseOver={() => {
+      console.log(title)
+    }}>
+      <img src={img} alt="" />
+      <h1 onClick={() => console.log(title)}>{title}</h1>
+      <h5>{author} </h5>
+      <button type='button' onClick={clickHandler}>referrence example</button>
+      <button type='button' onClick={() => complexExample(author)}>newbutton for authors</button>
+    </article>
+  )
+}
+
+
+
+ReactDom.render(<BookList />, document.getElementById('root'))
